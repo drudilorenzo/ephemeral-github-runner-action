8ee37847395b259ed17b7d25f87a49e491cc82d6
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-07-25 17:32:48 +0200

    Providers print.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ async function run() {
 
   // Simple check on provider, arch and goal.
   // There's no support for arm64 machine on gcp.
-  core.info(Object.values(providers));
+  core.info(Object.values(providers).toString());
   core.info("Checking the inputs...");
   if (!Object.values(providers).includes(config.cloudProvider.toLowerCase())) {
     core.setFailed("Wrong provider");
