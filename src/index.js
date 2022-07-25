8133068447a7486489a8a0d4c7c7547036661617
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-07-25 11:33:19 +0200

    Fix `index.js`.

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ async function run() {
      urlPrefix += githubToken + "@";
   } 
   urlPrefix += "github.com/";
-  const userRepoUrl = urlPrefix + github.context.payload.repository.name;
+  const userRepoUrl = urlPrefix + github.context.payload.repository;
   core.info(userRepoUrl)
   await exec.exec('git', ['clone', `${userRepoUrl}`]);
 
