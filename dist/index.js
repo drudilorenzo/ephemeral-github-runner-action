3d4bca14e1b8c12ac5ac62103388ba19e1f9580c
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-07-28 16:14:11 +0200

    Testing.

--- dist/index.js
+++ dist/index.js
@@ -10392,7 +10392,7 @@ async function deployRunners(CONFIG) {
     try {
         core.info("Deploying the runners...");
         await exec.exec('pulumi', ['login', `${CONFIG.pulumiBackendUrl}`], { cwd: CONFIG.runnerRepoPath });
-        await exec.exec('pulumi', ['stack', 'init', `${CONFIG.stackName}`, '--secrets-provider=passphrase'], { cwd: CONFIG.providerPath });
+        await exec.exec('pulumi', ['stack', 'init', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
         await exec.exec('pulumi', ['stack', 'select', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
         await exec.exec('pulumi', ['update', '--diff', '--config-file', `${CONFIG.configFilePath}`], { cwd: CONFIG.providerPath });
         core.info("Runners deployed!");
