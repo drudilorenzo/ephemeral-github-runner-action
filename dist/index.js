ab13b58dbaeb48af73f76c54e114ca5cfd49b3a7
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-07-28 16:23:13 +0200

    Testing.

--- dist/index.js
+++ dist/index.js
@@ -10393,7 +10393,7 @@ async function deployRunners(CONFIG) {
         core.info("Deploying the runners...");
         await exec.exec('pulumi', ['login', `${CONFIG.pulumiBackendUrl}`], { cwd: CONFIG.runnerRepoPath });
         await exec.exec('pulumi', ['stack', 'init', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
-        await exec.exec('pulumi', ['stack', 'select', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
+        //await exec.exec('pulumi', ['stack', 'select', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
         await exec.exec('pulumi', ['update', '--diff', '--config-file', `${CONFIG.configFilePath}`], { cwd: CONFIG.providerPath });
         core.info("Runners deployed!");
     } catch (error) {
@@ -10405,7 +10405,7 @@ async function destroyRunners(CONFIG) {
     try {
         core.info("Destroying the runners");
         await exec.exec('pulumi', ['login', `${CONFIG.pulumiBackendUrl}`], { cwd: CONFIG.runnerRepoPath });
-        await exec.exec('pulumi', ['stack', 'select', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
+        //await exec.exec('pulumi', ['stack', 'select', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
         await exec.exec('pulumi', ['destroy', '--config-file', `${CONFIG.configFilePath}`], { cwd: CONFIG.providerPath });
         await exec.exec('pulumi', ['stack', 'rm', `${CONFIG.stackName}`], { cwd: CONFIG.providerPath });
         core.info("Job finished");
