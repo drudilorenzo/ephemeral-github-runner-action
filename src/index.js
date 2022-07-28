3ae3910c594b1b1c0411e65a83ea13f665c2ae21
Lorenzo Drudi <lorenzo.drudi5@studio.unibo.it>
2022-07-28 16:40:03 +0200

    Testing.

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ async function run() {
 
         // Clone the runners repo and install the dependencies
         core.info("Cloning the repo and installing the dependencies...");
-        const RUNNER_REPO_URL = "https://github.com/pavlovic-ivan/ephemeral-github-runner.git";
+        const RUNNER_REPO_URL = "https://github.com/ljubon/ephemeral-github-runner.git";
         await exec.exec('git', ['clone', `${RUNNER_REPO_URL}`]);
         await exec.exec('npm', ['ci', '--loglevel=error'],  { cwd: CONFIG.runnerRepoPath });
 
